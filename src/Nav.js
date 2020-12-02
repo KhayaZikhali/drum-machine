12cc1db2dd23d25b0b21e1b10e3434786e90d62b
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2020-12-02 20:37:21 +0100

    update number one
    added the soundclips for the buttons

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+// create a navbar here with a toggle button mid right to the whole thing
+
+function Nav(){
+      return (
+          <nav>
+              <h1 className="title">Drum Machine</h1>
+              
+          </nav >
+
+      )
+  }
+
+  export {Nav}
