8d85c27a5678eed9c03d1a8f4d950d6b5dd4ebca
KhayaZikhali <70526289+KhayaZikhali@users.noreply.github.com>
2020-12-17 20:25:05 +0100

    Added soundbutes
    added soundbites
    streamlined the rendering of the buttons

--- src/button.js
+++ src/button.js
@@ -1,35 +1,38 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import flute1 from './bites/FLUTE1.mp3';
-import flute2 from './bites/FLUTE2.mp3';
-import flute3 from './bites/FLUTE3.mp3';
-import flute4 from './bites/FLUTE4.mp3';
-import saxy1 from './bites/SAXY1.mp3';
-import saxy2 from './bites/SAXY2.mp3';
-import saxy4 from './bites/SAXY4.mp3';
-import flute5 from './bites/FLUTE5.mp3';
+import CiHat01 from './bites/CiHat01.mp3';
+import CiHat02 from './bites/CiHat02.wav';
+import CiHat03 from './bites/CiHat03.wav';
+import Kick01  from './bites/Kick01.wav';
+import Kick02  from './bites/Kick02.wav';
+import Kick03  from './bites/Kick03.wav';
+import OpHat01 from './bites/OpHat01.wav';
+import OpHat02 from './bites/OpHat02.wav';
+import OpHat03 from './bites/OpHat03.wav';
 import {Howl , Howler} from 'howler';
 // import audio clips
 
 // create a label const for each of the sounds
+// audioclips for hte first column o fbuttons
 const audioclips = [
-  { sound : flute1 , label: "Q"},
-  { sound : flute2 , label: "A"},
-  { sound : flute3 , label: "Z"}
-]
-
-const audioclips2 = [
-  { sound : flute4 , label: "W"},
-  { sound : flute5 , label: "S"},
-  { sound : saxy1 , label: "X"}
-];
-
-const audioclips3 = [  
-  { sound : saxy2 , label: "E"},
-  { sound : saxy4 , label: "D"}
-]
+  { sound : CiHat01 , label: "Q" , name : "CiHat01"},
+  { sound : CiHat02 , label: "A", name : "CiHat02"},
+  { sound : CiHat03 , label: "Z" , name : "CiHat03"},]
   
+// audioclips for the second cilumn of buttons
+  const  wsx= [
+  { sound : Kick01 , label: "W" , name : "Kick01"},
+  { sound : Kick02 , label: "S", name : "Kick02"},
+  { sound : Kick03 , label: "X" , name : "Kick03"},
+]
+//audioclips for the third column of buttons  
+  const edc = [
+  { sound : OpHat01 , label: "E", name:"OpHat01"},
+  { sound : OpHat02 , label: "D", name:"OpHat02"},
+  { sound: OpHat03 ,label: "C" , name:"OpHat03"}
+]
+
 class Pad extends React.Component {
   constructor(props) {
     super(props);
@@ -37,45 +40,49 @@ class Pad extends React.Component {
       quote: '  ' 
     };
   }
-
+//function to play the sound from its src
   SoundPlay = (src)=> {
     const sound = new Howl ({
-      src
+      src 
     })
     sound.play();
   } 
   
-   //add eventlisteners
-   
+
+   // render a button element for the the first column of buttons   
    RenderButtonandName(){
      return audioclips.map((soundObj, index) => {  
        return (
-         <button class="btn btn-primary btn-xl" key ={index} onClick={() => this.SoundPlay(soundObj.sound)} onclick={()=>{this.setState(state => ({quote : "EEVRYBI"}));}}>
+         <button class="btn btn-primary btn-xl" key ={index} onClick={() => this.SoundPlay(soundObj.sound)}>
            {soundObj.label}
          </button>
        )
      })
-   }
-  
-   RenderButtonandNameE(){
-    return audioclips2.map((soundObj, index) => {  
-      return (
-        <button class="btn btn-primary btn-xl" key ={index} onClick={() => this.SoundPlay(soundObj.sound)} onclick={()=>{this.setState(state => ({quote : "EEVRYBI"}));}}>
-          {soundObj.label}
-        </button>
-      )
-    })
-  }
+   };
 
-  RenderButtonandNameR(){
-    return audioclips3.map((soundObj, index) => {  
-      return (
-        <button class="btn btn-primary btn-xl col-md-3" key ={index} onClick={() => this.SoundPlay(soundObj.sound)} onclick={()=>{this.setState(state => ({quote : "EEVRYBI"}));}}>
-          {soundObj.label}
-        </button>
-      )
-    })
-  }
+// render  buttons for the second column
+   RenderButtonandNamee(){
+     return wsx.map((soundObj, index) => {  
+       return (
+         <button class="btn btn-primary btn-xl" key ={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+           {soundObj.label}
+         </button>
+       )
+     })
+   };
+
+   //render buttons for the third column
+   RenderButtonandNamea(){
+     return edc.map((soundObj, index) => {  
+       return (
+         <button class="btn btn-primary btn-xl" key ={index} onClick={() => this.SoundPlay(soundObj.sound)}>
+           {soundObj.label}
+         </button>
+       )
+     })
+   };
+
+  /* ADD KEYBOARD EVENT HANDLERS */
 
   render() {
     Howler.volume(1.0)
@@ -86,22 +93,32 @@ class Pad extends React.Component {
         <div class="container-fluid" id="display">
           <h2 class="t">{this.state.quote}</h2>
         </div>
-      <div className="pad cols-3">
+      <div className="pad">
+        
         {/* First row of buttons in the pad */}
-        <div class="well">{this.RenderButtonandName()}</div>
-        <div class="well">{this.RenderButtonandNameE()}</div>
-        <div class="well">{this.RenderButtonandNameR()}</div>
+        <div class="well">
+          <div class="btn-xl">
+            {this.RenderButtonandName()}
+            </div>
+          </div>
+          
+          {/* Second column of buttons */}
+        <div class="well bg-midnight"><div class="btn-xl">
+          {this.RenderButtonandNamee()}
+          </div>
+        </div>
+
+        {/* Third column of buttons */}
+        <div class="well">
+          <div class="btn-xl">
+           {this.RenderButtonandNamea()}
+        </div>
+       </div>
       </div>
-      </div>
+     </div>
     );
   }
 }
-// display 
-export {Pad}
 
-         /*
-         1 a
-         
-         
-           */
-      
+// display 
+export {Pad}      
